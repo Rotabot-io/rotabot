d8c0860d5b839427d332671581906d745d2e29df
Kevin Robayna <me@kevinrobayna.com>
2023-10-03 10:36:38 +0100

    chore: Refactor db layer
    sqlc is amazing but because we want to have internal methods like save/update and have a exported method to handle them both we need our own interface and layer for this.
    
    We have now made everything use our repository so that use this abstraction across the repository (and have generated mock code to avoid calling the db)

--- lib/db/repository.go
+++ lib/db/repository.go
@@ -1,13 +1,9 @@
+//go:generate mockgen -package mock_db -destination=mock_db/db.go . Repository
 package db
 
 import (
 	"context"
 	"errors"
-
-	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgconn"
-	"github.com/rotabot-io/rotabot/lib/zapctx"
-	"go.uber.org/zap"
 )
 
 var (
@@ -15,43 +11,28 @@ var (
 	ErrNotFound      = errors.New("no rows in result set")
 )
 
-type CreateOrUpdateRotaParams struct {
-	RotaID    string
-	TeamID    string
-	ChannelID string
-	Name      string
-	Metadata  RotaMetadata
+// RotaSchedule is the type that defines how the members of a rota are scheduled
+type RotaSchedule string
+
+// RotaFrequency is the type that defines how long a rota lasts
+type RotaFrequency string
+
+const (
+	RFDaily   = RotaFrequency("Daily")
+	RFWeekly  = RotaFrequency("Weekly")
+	RFMonthly = RotaFrequency("Monthly")
+
+	RSCreated = RotaSchedule("Created At")
+	RSRandom  = RotaSchedule("Randomly")
+)
+
+type RotaMetadata struct {
+	Frequency      RotaFrequency `json:"frequency"`
+	SchedulingType RotaSchedule  `json:"scheduling_type"`
 }
 
-func CreateOrUpdateRota(ctx context.Context, tx pgx.Tx, p CreateOrUpdateRotaParams) (string, error) {
-	l := zapctx.Logger(ctx)
-	client := New(tx)
-	var rotaId string
-	var err error
-	if p.RotaID != "" {
-		rotaId, err = client.UpdateRota(ctx, UpdateRotaParams{
-			ID:       p.RotaID,
-			Name:     p.Name,
-			Metadata: p.Metadata,
-		})
-	} else {
-		rotaId, err = client.SaveRota(ctx, SaveRotaParams{
-			Name:      p.Name,
-			TeamID:    p.TeamID,
-			ChannelID: p.ChannelID,
-			Metadata:  p.Metadata,
-		})
-	}
-	if err != nil {
-		var pgError *pgconn.PgError
-		if errors.As(err, &pgError) {
-			switch pgError.Code {
-			case "23505":
-				return "", ErrAlreadyExists
-			}
-		}
-		l.Error("failed to save rota", zap.Error(err))
-		return "", err
-	}
-	return rotaId, nil
+type Repository interface {
+	CreateOrUpdateRota(ctx context.Context, p CreateOrUpdateRotaParams) (string, error)
+	FindRotaByID(ctx context.Context, id string) (Rota, error)
+	ListRotasByChannel(ctx context.Context, args ListRotasByChannelParams) ([]Rota, error)
 }
